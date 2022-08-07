b93d904cca21abb4e2a66f9b9ae2ad4bee414d3e
joel <>
2022-08-08 00:14:07 +0800

    fix login

--- src/components/layout/LayoutContainer.jsx
+++ src/components/layout/LayoutContainer.jsx
@@ -1,6 +1,6 @@
 const LayoutContainer = ({ children }) => {
 	return (
-		<div className="h-screen w-screen max-h-screen overflow-auto flex">
+		<div className="h-screen w-screen max-h-screen overflow-auto flex bg-slate-200">
 			{children}
 		</div>
 	);
