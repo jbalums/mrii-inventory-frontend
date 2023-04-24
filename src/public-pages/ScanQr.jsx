59535b4fab7e9404cdad3cc80b1eb8da61bf8d76
Joel Baluma <baluma.joel91@gmail.com>
2023-04-24 15:05:22 +0800

    test qr 1

--- src/public-pages/ScanQr.jsx
+++ src/public-pages/ScanQr.jsx
@@ -1,47 +1,18 @@
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 import FlatIcon from "../components/FlatIcon";
 import PublicAppLayout from "../components/PublicAppLayout";
 import { QrReader } from "react-qr-reader";
 const ScanQr = () => {
-	const [scannedData, setScannedData] = useState("");
-	const videoRef = useRef();
-
-	const handleScan = (event) => {
-		const video = videoRef.current;
-		const canvas = document.createElement("canvas");
-		const context = canvas.getContext("2d");
-
-		// Draw video frame onto canvas
-		context.drawImage(video, 0, 0, canvas.width, canvas.height);
-
-		// Get image data from canvas
-		const imageData = context.getImageData(
-			0,
-			0,
-			canvas.width,
-			canvas.height
-		);
-
-		// Decode QR code from image data
-		const qr = new window.QRCodeDecoder();
-		const result = qr.decodeFromImage(imageData);
-
-		// Set scanned data
-		setScannedData(result);
+	const [data, setData] = useState("No result");
+	const handleScan = (data) => {
+		if (data) {
+			alert(`Scanned QR code with data: ${data}`);
+		}
 	};
 
 	const handleError = (error) => {
 		console.error(error);
 	};
-
-	useEffect(() => {
-		navigator.mediaDevices
-			.getUserMedia({ video: { facingMode: "environment" } })
-			.then((stream) => {
-				videoRef.current.srcObject = stream;
-			})
-			.catch(handleError);
-	}, []);
 
 	return (
 		<PublicAppLayout
@@ -56,18 +27,24 @@ const ScanQr = () => {
 				},
 			]}
 		>
-			<div>
-				{scannedData ? (
-					<p>Scanned data: {scannedData}</p>
-				) : (
-					<video
-						ref={videoRef}
-						style={{ width: "100%", height: "auto" }}
-						onPlay={handleScan}
-						onError={handleError}
-					/>
-				)}
-			</div>
+			<QrReader
+				facingMode="environment"
+				onResult={(result, error) => {
+					if (!!result) {
+						setData(result?.text);
+					}
+
+					if (!!error) {
+						console.info(error);
+					}
+				}}
+				style={{
+					width: "100%",
+					background: "red",
+					border: "1px solid black",
+				}}
+			/>
+			<p>{data}</p>
 		</PublicAppLayout>
 	);
 };
