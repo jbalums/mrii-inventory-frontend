5a2321d708af64e29f0e2c80298db69bd3c20f41
Joel Baluma <baluma.joel91@gmail.com>
2023-04-29 18:15:05 +0800

    updates

--- src/components/widgets/DashboardWidget.jsx
+++ src/components/widgets/DashboardWidget.jsx
@@ -6,17 +6,17 @@ const DashboardWidget = ({ bgColor, color, title, count, icon }) => {
 			className={`rounded-lg flex flex-col relative ${bgColor} aspect-[3/1] `}
 		>
 			<div
-				className={`font-semibold px-4 text-xs 2xl:text-lg pt-3 ${color}`}
+				className={`font-semibold px-4 text-xs text-left 2xl:text-lg pt-3 ${color}`}
 			>
 				{title}
 			</div>
-			<div className="px-4 pt-11 pb-5 flex items-center justify-start">
-				<h2 className={`text-[88px] font-bold ${color}`}>{count}</h2>
+			<div className="px-4 pt-6 pb-6 flex items-center justify-start">
+				<h2 className={`text-[62px] font-bold ${color}`}>{count}</h2>
 			</div>
-			<div className="absolute top-4 right-4 2xl:right-6 opacity-20">
+			<div className="absolute top-11 right-4 2xl:right-6 opacity-20">
 				<FlatIcon
 					icon={icon}
-					className={`text-[52px] 2xl:text-[72px] ${color}`}
+					className={`text-[52px] 2xl:text-[52px] ${color}`}
 				/>
 			</div>
 		</div>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -7,7 +7,7 @@ import DashboardWidget from "../components/widgets/DashboardWidget";
 const Dashboard = () => {
 	return (
 		<AppLayout title="Dashboard">
-			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-6 mb-6">
+			<div className="grid grid-cols-1 justify-center md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 mb-6">
 				<div className="">
 					<DashboardWidget
 						bgColor="border border-blue-600 bg-blue-600 bg-opacity-5"
