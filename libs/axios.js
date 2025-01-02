9ed3658f0081edfbf18fb454e0d9c6f07d838f3e
Your Name <you@example.com>
2025-01-02 10:45:11 +0800

    added config for old db

--- libs/axios.js
+++ libs/axios.js
@@ -4,9 +4,10 @@ const axios = Axios.create({
 	baseURL: import.meta.env.VITE_BACKEND_URL,
 	headers: {
 		"X-Requested-With": "XMLHttpRequest",
-		'X-Db-Config': 'mrii_prod_2024',
+		"X-Db-Config": import.meta.env.VITE_API_DB,
 	},
 	withCredentials: true,
 });
+
 
 export default axios;
