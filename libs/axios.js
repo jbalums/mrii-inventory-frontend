fa49d56c28caeb462839a45bb637e52080e15655
Your Name <you@example.com>
2025-01-02 10:38:55 +0800

    old db

--- libs/axios.js
+++ libs/axios.js
@@ -4,6 +4,7 @@ const axios = Axios.create({
 	baseURL: import.meta.env.VITE_BACKEND_URL,
 	headers: {
 		"X-Requested-With": "XMLHttpRequest",
+		'X-Db-Config': 'mrii_prod_2024',
 	},
 	withCredentials: true,
 });
